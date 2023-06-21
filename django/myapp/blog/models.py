2ee4543fc633527574981c3d0318174571f07c69
SonSuha <dhkd98@gmail.com>
2023-06-21 16:30:51 +0900

    daily

--- django/myapp/blog/models.py
+++ django/myapp/blog/models.py
@@ -5,10 +5,10 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
     writer = models.CharField(max_length=10)
-    create_at = models.DateField(auto_now_add=True)
+    created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
     content = models.TextField()
     writer = models.CharField(max_length=10)
-    create_at = models.DateField(auto_now_add=True)
+    created_at = models.DateField(auto_now_add=True)
--- django/myapp/blog/urls.py
+++ django/myapp/blog/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from . import views
-from blog.views import Index
+
+app_name = 'blog'
 
 urlpatterns = [
-    #path(패턴, 매핑)
+    #path(패턴, 매핑) /blog/
     # path("", views.index) # FBV
-    path("", Index.as_view()) #CBV
-    # 글 조회
+    # 글 목록 조회
+    path("", views.List.as_view(), name = 'list'), #CBV라서 as view 붙임 /blog/
+    # 글 상세 조회
+    path("detail/<int:pk>", views.Detail.as_view(), name = 'detail'), # /blog/detail/1
     # 글 작성
+    path('write/', views.Write.as_view(), name = 'write'), # /blog/write/
     # 글 수정
     # 글 삭제
     # 코멘트 작성
