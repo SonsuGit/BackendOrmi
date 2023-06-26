fa1d0c12103136f61d482a62e2f7be6279acc718
SonSuha <dhkd98@gmail.com>
2023-06-26 15:50:37 +0900

    daily

--- django/myapp/user/views.py
+++ django/myapp/user/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views import View
+from django.contrib.auth import authenticate, login, logout
+from .models import User
+from .forms import RegisterForm, LoginForm
 
 # Create your views here.
+# user 관련된 기능
+# 회원가입
+# 로그인
+# 로그아웃
+
+### Registration
+class Registration(View):
+    # 버튼 눌렀을때 작성페이지
+    def get(self, request):
+        # 회원 가입 페이지
+        # 정보를 입력할 폼을 보여주어야 한다.
+        form = RegisterForm()
+        context = {
+            'form' : form
+        }
+        return render(request, 'user/user_register.html', context)
+
+    # 작성 다 하고나서 보내기
+    def post(self, request):
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('blog:list')
+
+
+### Login
+class Login(View):
+    def get(self, request):
+        form = LoginForm()
+        context = {
+            'form' : form
+        }
+        return render(request, 'user/user_login.html', context)
+
+    def post(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(username=email, password=password) # True, False
+
+            if user:
+                login(request, user)
+                return('blog:list')
+
+            form.add_error(None, '아이디가 없습니다.')
+
+        context = {
+            'form' : form
+        }
+
+        return render(request, 'user/user_login.html', context)
+
+
+### Logout
+class Logout(View):
+    def get(self, request):
+        logout(request)
+        return redirect('blog:list')
